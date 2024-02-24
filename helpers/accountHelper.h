6250730f08995be4a53cfd279f6f8aae68a20bf4
Lukasz Babis <babislukaszstudy@gmail.com>
2024-02-24 21:21:11 +0100

    Small adjustment
    Deleted CurrentData prototype from accountHelper.h, as it doesn't seem necessary

--- helpers/accountHelper.h
+++ helpers/accountHelper.h
@@ -10,7 +10,7 @@ using namespace std;
 
 void ShowAccount(fstream &account_file);
 void NewAccountEntry(fstream &account_file, int account_amount, tm t);
-tm CurrentDate();
 void FindAccountBalance(fstream &account_file, string date);
 
 #endif
+
